f50360b88a4578c9281522fc6f6765cadd788bd4
DewDoughnut <45787842+DewDoughnut@users.noreply.github.com>
2019-12-12 14:24:04 +0000

    updating UI stuff

---
+++ Assets/Test_health_loss.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test_health_loss : MonoBehaviour
+{
+
+    void OnMouseDown()
+    {
+        Debug.Log("You have clicked the square");
+        GetComponent<health_bar_number>();        
+    }
+}
