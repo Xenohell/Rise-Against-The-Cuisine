fb31118b80a72927ad7f671812505cc1227f8364
DewDoughnut <45787842+DewDoughnut@users.noreply.github.com>
2020-01-30 17:53:57 +0000

    stuff and things

--- Assets/UI/health_bar_number.cs
+++ Assets/UI/health_bar_number.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class health_bar_number : MonoBehaviour
 {
     public Text p_health_Text;
+    public Text GameOverText;
     public Slider p_health_bar_slider;
     public float p_current_health;
     public float p_max_health;
     public GameObject testbox;
+    public bool GameOverBool = false;
     void Start()
     {
         p_current_health = p_max_health;
@@ -17,17 +19,23 @@ public class health_bar_number : MonoBehaviour
 
 
     }
-    
+
     // Start is called before the first frame update
     // Update is called once per frame
     void Update()
     {
         p_health_bar_slider.value = ChangeHealth();
         p_health_Text.text = "" + (Mathf.Round(p_current_health)) + "%";
-
-        if(p_current_health <= 0)
+        if (p_current_health >= 1)
         {
+            GameOverText.text = "";
+        }
+
+        if (p_current_health <= 0 && !GameOverBool)
+        {
+            GameOverBool = true;
             Destroy(testbox);
+            GameOver();
         }
     }
     float ChangeHealth()
@@ -35,5 +43,18 @@ public class health_bar_number : MonoBehaviour
 
         return p_current_health / p_max_health;
     }
+    void GameOver()
+    {
+        if (GameOverBool == true)
+        {
+            string[] dead = new string[]
+             {"You're Toast", "You're Swiss Cheese",
+         "Roasted", "Consumed", "Smoked", "Sliced", "Diced",
+         "You've been Fried", "Smoked Out",
+         "Burned", "Someone's Cooking Tonignt",
+         "You're cooked", "Consumed", "You're Someones Dinner Now",
+         "Roasty Toasty"};
+            GameOverText.text = dead[Random.Range(0, dead.Length - 1)];
+        }
+    }
 }
-        
