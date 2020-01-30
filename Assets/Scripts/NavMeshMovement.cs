9aa14173cdf9db315dcb3333fb9850f1668308a4
Xenohell <vodnarana@yahoo.com>
2020-01-30 14:43:49 +0000

    AI movement

---
+++ Assets/Scripts/NavMeshMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Transform target;
+
+    public float stoppingRadius = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(target.position, transform.position);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation
+                                              , Quaternion.LookRotation(target.position - transform.position)
+                                              , 5 * Time.deltaTime);
+
+        
+        agent.SetDestination(target.position);
+        
+    }
+}
