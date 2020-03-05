b0a11f26716a93ac0265e203821bccf8e16d4e57
DewDoughnut <45787842+DewDoughnut@users.noreply.github.com>
2020-03-05 17:01:58 +0000

    changes to my thing

---
+++ Assets/Menu_scene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_scene : MonoBehaviour
+{
+    public string level;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
--- Assets/UI/health_bar_number.cs
+++ Assets/UI/health_bar_number.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class health_bar_number : MonoBehaviour
 {
-    public Text p_health_Text;
-    public Text GameOverText;
-    public Slider p_health_bar_slider;
-    public float p_current_health;
-    public float p_max_health;
-    public GameObject testbox;
-    public bool GameOverBool = false;
+    public TextMeshProUGUI p_health_Text; //this is the text of the health
+    public TextMeshProUGUI GameOverText; // this is the game over text
+
+    public Slider p_health_bar_slider; // the healthbar slider only works between 1 and 0 so works as decimal values
+    public float p_current_health; //this is the current health
+    public float p_max_health; // max health. current health cant go over max health unless changed outside of game
+    public GameObject testbox; // to be changed to player
+
+    public bool GameOverBool = false; // and its a boolean for when you die the game isnt over
     void Start()
     {
-        p_current_health = p_max_health;
-        p_health_bar_slider.value = ChangeHealth();
+        p_current_health = p_max_health; //on start current health = max health
+        p_health_bar_slider.value = ChangeHealth(); //the health bar slider value is what the change health says
 
 
     }
@@ -24,11 +27,11 @@ public class health_bar_number : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        p_health_bar_slider.value = ChangeHealth();
-        p_health_Text.text = "" + (Mathf.Round(p_current_health)) + "%";
+        p_health_bar_slider.value = ChangeHealth(); //slider value is what change health says
+        p_health_Text.text = "" + (Mathf.Round(p_current_health)) + "%"; // the health bar text is a rounded up version of 
         if (p_current_health >= 1)
         {
-            GameOverText.text = "";
+            GameOverText.text = " ";
         }
 
         if (p_current_health <= 0 && !GameOverBool)
@@ -41,7 +44,7 @@ public class health_bar_number : MonoBehaviour
     float ChangeHealth()
     {
 
-        return p_current_health / p_max_health;
+        return p_current_health / p_max_health; //your health is = to current health/ max health as it needs to work as a percentage for the slider
     }
     void GameOver()
     {
