1932ed3411647d3ed8a3dd57474bfaee492d5789
Xenohell <vodnarana@yahoo.com>
2020-03-26 16:59:27 +0000

    Level 2 enemies

--- Assets/Scripts/EnemySpiderMovment.cs
+++ Assets/Scripts/EnemySpiderMovment.cs
@@ -17,11 +17,11 @@ public class EnemySpiderMovment : MonoBehaviour
 
     void Start()
     {
+        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         //This sets the player value
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         //This sets the home value to it's spawn point
         
-
         currentHealth = startHealth;
 
         //This sets the target to the player
@@ -48,14 +48,16 @@ public class EnemySpiderMovment : MonoBehaviour
         if (currentHealth <= 30)
         {
             target = home.transform;
-            agent.speed = 5f;
+            agent.speed = 1.5f;
+            agent.stoppingDistance = 0;
         }
 
         if (currentHealth >= 100)
         {
             //Debug.Log("Attack!");
             target = player;
-            agent.speed = 3.5f;
+            agent.speed = 0.5f;
+            agent.stoppingDistance = 2;
         }
 
         if (dist > agent.stoppingDistance)
@@ -72,20 +74,14 @@ public class EnemySpiderMovment : MonoBehaviour
             currentHealth -= Random.Range(8, 14);
         }
 
-        // If the enemy's health is equal to 0 or drops below 0, call the Death method
-        //if (currentHealth <= 0)
-            //Death();
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Home"))
+        if (collision.gameObject.tag == "Home")
         {
             currentHealth = 100;
         }
+
+        // If the enemy's health is equal to 0 or drops below 0, call the Death method
+        //if (currentHealth <= 0)
+        //Death();
     }
-
-
-
 
 }
